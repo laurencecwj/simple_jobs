be61c0819f5e05b83e2b976fbaa085b0381b5bff
laurencecwj <laurence@c12.ai>
2025-03-27 13:45:37 +0800

    add skypilot files

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ OLDPATH=`pwd`
 cd $BASEDIR
 WORKDIR=`pwd`
 
-echo "current node: ${SKYPILOT_SETUP_NODE_RANK}"
-docker run --rm -it -v $WORKDIR:/workspace/simple_jobs rust:slim-bullseye bash /workspace/simple_jobs/local.sh ${SKYPILOT_SETUP_NODE_RANK}
+echo "current node: "`cat $WORKDIR/node.id`
+docker run --rm -it -v $WORKDIR:/workspace/simple_jobs rust:slim-bullseye bash /workspace/simple_jobs/local.sh `cat /workspace/simple_jobs/node.id`
